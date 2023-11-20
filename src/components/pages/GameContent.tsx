34346c187396543ce378e9aaa8a400dd451e9acd
victorkummell1 <victorkummell@gmail.com>
2023-11-20 17:10:13 -0300

    📦 GET Image from DataBase

--- src/components/pages/GameContent.tsx
+++ src/components/pages/GameContent.tsx
@@ -3,7 +3,7 @@ import { Game } from '../shared/GamePage'
 export default function GameContent({ params }: { params: { slug: string } }) {
   return (
     <main className="bg-assasins-creed-mirage bg-no-repeat bg-contain bg-top text-white">
-      <div className="w-full bg-gradient-to-t from-25% from-dark-10 flex flex-col items-center justify-start py-24 gap-12">
+      <div className="w-full flex flex-col items-center justify-start py-24 gap-12">
         <Game.Root>
           <div className="flex flex-col justify-center gap-[100px]">
             <section className="flex flex-col justify-center gap-[30px]">
--- src/components/shared/GamePage/GameSlide.tsx
+++ src/components/shared/GamePage/GameSlide.tsx
@@ -12,6 +12,13 @@ export async function GameSlide({ params }: { params: { slug: string } }) {
       product_slug: products?.slug,
     },
   })
+  const thumbImage = await db.image.findMany({
+    where: {
+      product_slug: products?.slug,
+    },
+  })
+
+  const isImages = thumbImage.length > 0
 
   return (
     <section className="w-[800px] max-w-[800px] h-[578px] max-h-[578px] flex flex-col gap-7">
@@ -22,31 +29,45 @@ export async function GameSlide({ params }: { params: { slug: string } }) {
         alt="Assassins Creed Mirage Game Image"
         className="w-[800px] h-[450px]"
       />
-      <div className="w-full h-[100px] flex items-center justify-between overflow-hidden">
-        <Image
-          src={images?.url || 'https://i.imgur.com/2VL43uD.png'}
-          width={182}
-          height={100}
-          alt="Assassins Creed Mirage Game Image 3"
-        />
-        <Image
-          src={images?.url || 'https://i.imgur.com/2VL43uD.png'}
-          width={182}
-          height={100}
-          alt="Assassins Creed Mirage Game Image 1"
-        />
-        <Image
-          src={images?.url || 'https://i.imgur.com/2VL43uD.png'}
-          width={182}
-          height={100}
-          alt="Assassins Creed Mirage Game Image 2"
-        />
-        <Image
-          src={images?.url || 'https://i.imgur.com/2VL43uD.png'}
-          width={182}
-          height={100}
-          alt="Assassins Creed Mirage Game Image 4"
-        />
+      <div className="w-full h-[100px] flex items-center justify-start gap-6 overflow-hidden">
+        {isImages ? (
+          thumbImage.map((image, index) => (
+            <Image
+              key={index}
+              src={image.url || 'https://i.imgur.com/2VL43uD.png'}
+              width={182}
+              height={100}
+              alt={images?.alt || 'Image Not Found'}
+            />
+          ))
+        ) : (
+          <div className="w-full flex items-center justify-start gap-6">
+            <Image
+              src="https://i.imgur.com/2VL43uD.png"
+              width={182}
+              height={100}
+              alt="Image Not Found"
+            />
+            <Image
+              src="https://i.imgur.com/2VL43uD.png"
+              width={182}
+              height={100}
+              alt="Image Not Found"
+            />
+            <Image
+              src="https://i.imgur.com/2VL43uD.png"
+              width={182}
+              height={100}
+              alt="Image Not Found"
+            />
+            <Image
+              src="https://i.imgur.com/2VL43uD.png"
+              width={182}
+              height={100}
+              alt="Image Not Found"
+            />
+          </div>
+        )}
       </div>
     </section>
   )
